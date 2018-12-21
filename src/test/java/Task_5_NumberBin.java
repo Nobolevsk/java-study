f3d42a4718eed76f1948955cfa385712f16468e7
nobolevsk <nobolevsk@gmail.com>
2018-12-21 19:00:17 +0300

    Выполненное задание №5

--- src/test/java/Task_5_NumberBin.java
+++ src/test/java/Task_5_NumberBin.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Task_5_NumberBin {
     public static void main(String[] args) {
         // Ввести с консоли число в бинарном формате. Перевести его в int и вывести на экран
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Введите число в бинарном формате: ");
+        String b = scanner.nextLine();
+
+        int q = Integer.parseInt(b, 2);
+        System.out.println("Бинарное число в формате int = "+q);
 
     }
 }
