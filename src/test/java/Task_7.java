cd3a2ba6a61e6e4a3f6b4bf7ed8892886ae3a57d
nobolevsk <nobolevsk@gmail.com>
2018-12-24 18:35:03 +0300

    Выполненное задание №7

---
+++ src/test/java/Task_7.java
@@ -0,0 +1,11 @@
+/*Задача №7: Цикличный вывод данных на экран Задание
+        Необходимо с помощью цикла вывести на экран числа от 1 до 50.*/
+
+public class Task_7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
