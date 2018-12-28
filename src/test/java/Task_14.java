19abda6d73ec4b1e6be7fe43f8b708386d0ad9f1
nobolevsk <nobolevsk@gmail.com>
2018-12-28 14:50:36 +0300

    Выполненное задание №14

--- src/test/java/Task_14.java
+++ src/test/java/Task_14.java
@@ -6,13 +6,19 @@ import java.util.Arrays;
 
 public class Task_14 {
     public static void main(String[] args) {
-        int w = 1 + (int) (Math.random()*10);
-        int[] a = new int[w];
+        int[] a = new int[5]; // объявление массива а
 
-        for (int i = a.length-1; i > 0; i--) {
-            a[i] = i;
-            for (int j = 0; j < i; j++) {
-                if (a[j]>a[j+1]){
+        for (int k = 0; k < a.length; k++) { //цикл k для заполнения массива
+            int w = 4 + (int) (Math.random()*10);// рандомное число
+            a[k] = w;
+        }
+        System.out.println("Первоначальных вид массива \n" + Arrays.toString(a)+"\n");// вывод массива
+        System.out.println("Процесс сортировки массива пузырьковым методом↓");
+        for (int i = a.length-1; i > 0; i--) { // цикл для перебора строк в массиве а
+            //a[i] = i;
+            for (int j = 0; j < i; j++) { // цикл для перебора столбцов(значений) в массиве а
+                //a[j] = j;
+                if (a[j]>a[j+1]){ // условие для пузырьковой сортировки
                     int v = a[j];
                     a[j] = a[j+1];
                     a[j+1] = v;
@@ -20,5 +26,6 @@ public class Task_14 {
             }
             System.out.println(Arrays.toString(a));
         }
+        System.out.println("\nОтсортированный массив имеет финальный вид \n" + Arrays.toString(a)); // вывод результата массива а
     }
 }
