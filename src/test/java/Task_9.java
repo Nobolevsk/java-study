e3316d7ae10be2e5e442bd81661c54daf27c2ce1
nobolevsk <nobolevsk@gmail.com>
2019-01-09 11:00:26 +0300

    Исправленное задание №9

--- src/test/java/Task_9.java
+++ src/test/java/Task_9.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /** Задание №9: Ввод/вывод данных для матрицы
@@ -7,19 +8,27 @@ import java.util.Scanner;
 public class Task_9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 2 числа для матрицы: ");
+        /*System.out.println("Введите 2 числа для матрицы: ");
         int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int b = scanner.nextInt();*/
 
-        int mas[][] = new int[a][b];
+        int mas[][] = new int[3][3];
+        System.out.println("Введите строки матрицы");
+        mas[0][0] = scanner.nextInt();
+        mas[0][1] = scanner.nextInt();
+        mas[0][2] = scanner.nextInt();
+        System.out.println("Введите столбец матрицы");
+        mas[0][0] = scanner.nextInt();
+        mas[1][0] = scanner.nextInt();
+        mas[2][0] = scanner.nextInt();
+
         for (int i = 0; i < mas.length; ++i) {
             for (int j = 0; j < mas[i].length; ++j) {
-                mas[i][j] = (i*j)+i+j+1;
+                //mas[i][j] = (i*j)+i+j+1;
                // System.out.print(mas[i][j] + " ");
             }
             int row = mas[i][0];
             System.out.print(row*3 + " ");
-            //System.out.println();
         }
     }
 }
