02571e787c37aa3848f5c3bca78c1befd52ee50a
nobolevsk <nobolevsk@gmail.com>
2019-01-21 16:47:44 +0300

    Выполненное задание №17

--- src/test/java/Task_17.java
+++ src/test/java/Task_17.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 /** Задача №17: Выгрузка данных из файла, проверка и перезапись.
  Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
@@ -7,33 +8,25 @@ import java.io.*;
 
 public class Task_17 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String file;
-        String consol;
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\user\\Documents\\programming\\gitrepo\\java-study\\src\\test\\resources\\file3.txt"));
-
-            int count = 1;
+            int count = 0;
             while ((file = reader.readLine()) != null) {
                 System.out.println(file);
                 count++;
             }
 
-            BufferedReader reader2 = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\user\\Documents\\programming\\gitrepo\\java-study\\src\\test\\resources\\file3.txt", true));
-
+            BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\user\\Documents\\programming\\gitrepo\\java-study\\src\\test\\resources\\file3.txt", false));
             System.out.println("Введите текст для перезаписи: ");
-
-            int count2 = 1;
-            if (reader2.readLine() != null)
-                count2++;
-
-            while ((consol = reader2.readLine()) != null){
-                if (count > count2){
-                    writer.write(consol);l
-                } else break;
+            for (int i = 0; i < count; i++) {
+                writer.write(scanner.nextLine());
+                writer.newLine();
             }
 
+            scanner.close();
             reader.close();
             writer.close();
         } catch (IOException e) {
